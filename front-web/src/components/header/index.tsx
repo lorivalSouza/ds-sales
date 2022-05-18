a3f79b767fa2ff1bdfd2b78ba9221c14dda593b6
lorivalSouza <lorivalnew@gmail.com>
2022-05-18 09:54:08 -0300

    First configurations and add Header component

---
+++ front-web/src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+function Header() {
+  return (
+    <header className="main-header-container">
+      <h1 className="main-header--title">DS Sales</h1>
+    </header>
+  );
+}
+
+export default Header;
