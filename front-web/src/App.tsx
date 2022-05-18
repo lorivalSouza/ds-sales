71f82d5ac968443cf35bd4c756d70846ca01390b
lorivalSouza <lorivalnew@gmail.com>
2022-05-18 11:28:07 -0300

    Add Filter component with FlatPickerDate

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
+import Filter from './components/filter';
 
 function App() {
   return (
     <>
       <Header />
+      <div className="app-container">
+        <Filter />
+      </div>
     </>
   );
 }
