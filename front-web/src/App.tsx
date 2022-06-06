47365167541cf6952ade0a9a6fe5af77b06ad9eb
lorivalSouza <lorivalnew@gmail.com>
2022-06-06 11:22:33 -0300

    Integrate Filter with App

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -5,14 +5,23 @@ import SalesByDate from './components/sales-by-date';
 import SalesSummary from './components/sales-summary';
 import PieChartCard from './components/pie-chart-card';
 import SalesTable from './components/sales-table';
+import { useState } from 'react';
+import { FilterData } from './types';
 
 function App() {
+  const [filterData, setFilterData] = useState<FilterData>();
+
+  const onFilterChange = (filter: FilterData) => {
+    setFilterData(filter);
+    console.log({ filter });
+  };
+
   return (
     <>
       <Header />
       <div className="app-container">
-        <Filter />
-        <SalesByDate />
+        <Filter onFilterChange={onFilterChange} />
+        <SalesByDate filterData={filterData} />
         <div className="sales-overview-container">
           <SalesSummary />
           <PieChartCard
